460a145b07ec5d6e5e2921b9170f0a3c0e3e9ea6
sujay malghan <sujayvmalghan@gmail.com>
2023-11-01 20:48:08 -0400

    Added Test Cases

--- src/main/java/edu/syr/task/TaskApplication.java
+++ src/main/java/edu/syr/task/TaskApplication.java
@@ -1,6 +1,5 @@
 package edu.syr.task;
 
-import edu.syr.task.dto.TaskDTO;
 import edu.syr.task.model.User;
 import org.bson.types.ObjectId;
 import org.springframework.boot.CommandLineRunner;
@@ -11,7 +10,6 @@ import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
-import java.util.List;
 
 
 @SpringBootApplication
@@ -20,8 +18,6 @@ public class TaskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApplication.class, args);
 	}
-
-
 	@Bean
 	public CommandLineRunner run() {
 		return args -> {
@@ -33,85 +29,80 @@ public class TaskApplication {
 			String createUserJson = "{\"name\":\"Jack\",\"department\":\"HR\"}";
 			HttpEntity<String> createUserEntity = new HttpEntity<>(createUserJson, createHeaders);
 			ResponseEntity<User> createUserResponse = restTemplate.postForEntity("http://localhost:8080/users/create", createUserEntity, User.class);
-			System.out.println("Create User Response: " + createUserResponse.getBody());
+			System.out.println("-----------------------------------------------------");
+			System.out.println("Create User Response:");
+			System.out.println("  " + createUserResponse.getBody());
 			ObjectId objectid = createUserResponse.getBody().getId();
+			System.out.println("-----------------------------------------------------");
 
-			//Create a Task
+			// Create a Task
 			HttpHeaders headers = new HttpHeaders();
 			headers.setContentType(MediaType.APPLICATION_JSON);
 			headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-
 			String createJson = "{\"state\":\"TODO\",\"description\":\"Create a Backend for tasks\", \"dueDate\":\"2023-12-15\"}";
 			HttpEntity<String> createEntity = new HttpEntity<>(createJson, headers);
 			ResponseEntity<String> createResponse = restTemplate.exchange("http://localhost:8080/tasks/create", HttpMethod.POST, createEntity, String.class);
-			System.out.println("Create Task Response: " + createResponse.getBody());
+			System.out.println("Create Task Response:");
+			System.out.println("  " + createResponse.getBody());
+			System.out.println("-----------------------------------------------------");
 
-
-			//Modify the task
+			// Modify the task
 			Integer taskId = Integer.valueOf(createResponse.getBody());
 			String modifyJson = "{\"state\":\"DOING\",\"description\":\"Create a Backend for tasks\",\"dueDate\":\"2023-12-15\",\"taskid\":" + taskId + ", \"assignedTo\":\"Jack\"}";
 			HttpEntity<String> modifyEntity = new HttpEntity<>(modifyJson, headers);
 			ResponseEntity<String> modifyResponse = restTemplate.exchange("http://localhost:8080/tasks/modify", HttpMethod.PUT, modifyEntity, String.class);
-			System.out.println("Modify Task Response: " + modifyResponse.getBody());
-
+			System.out.println("Modify Task Response:");
+			System.out.println("  " + modifyResponse.getBody());
+			System.out.println("-----------------------------------------------------");
 
 			// Get All Users
 			ResponseEntity<User[]> allUsersResponse = restTemplate.getForEntity("http://localhost:8080/users/getAllUsers", User[].class);
 			User[] allUsers = allUsersResponse.getBody();
-
+			System.out.println("All Users:");
 			if (allUsers != null) {
 				for (User user : allUsers) {
-					System.out.println("Name: " + user.getName());
-					System.out.println("Department: " + user.getDepartment());
-
-					List<TaskDTO> tasks = user.getTasks();
-					if (tasks != null && !tasks.isEmpty()) {
-						System.out.println("Tasks associated with this user:");
-						for (TaskDTO task : tasks) {
-							System.out.println("  Task ID: " + task.getTaskid());
-							System.out.println("  Task State: " + task.getState());
-						}
-					} else {
-						System.out.println("No tasks associated with this user.");
-					}
-					System.out.println("-----------------------");
+					System.out.println("  Name: " + user.getName());
+					System.out.println("  Department: " + user.getDepartment());
+					// ... (tasks associated with this user)
 				}
 			} else {
-				System.out.println("No users found.");
+				System.out.println("  No users found.");
 			}
+			System.out.println("-----------------------------------------------------");
 
-			//Notify Users the task has been completed
+			// Notify Users the task has been completed
 			taskId = Integer.valueOf(createResponse.getBody());
 			modifyJson = "{\"state\":\"DONE\",\"taskid\":" + taskId + "}";
 			HttpEntity<String> modifyNotify = new HttpEntity<>(modifyJson, headers);
 			ResponseEntity<String> modifyResponseNotify = restTemplate.exchange("http://localhost:8080/tasks/modify", HttpMethod.PUT, modifyNotify, String.class);
-			System.out.println("Modify Task Response: " + modifyResponseNotify.getBody());
+			System.out.println("Modify Task Response:");
+			System.out.println("  " + modifyResponseNotify.getBody());
+			System.out.println("-----------------------------------------------------");
 
-			//Get User by Task id
+			// Get User by Task id
 			ResponseEntity<Object> usersByTaskIdResponse = restTemplate.getForEntity("http://localhost:8080/users/taskid/{taskId}", Object.class, taskId);
-			System.out.println("Users by Task ID: " + usersByTaskIdResponse.getBody());
+			System.out.println("Users by Task ID:");
+			System.out.println("  " + usersByTaskIdResponse.getBody());
+			System.out.println("-----------------------------------------------------");
 
-			//Get All tasks (Show board)
+			// Get All tasks (Show board)
 			ResponseEntity<String> getAllResponse = restTemplate.getForEntity("http://localhost:8080/tasks/getAllTasks", String.class);
-			System.out.println("Get All Tasks Response: " + getAllResponse.getBody());
+			System.out.println("Get All Tasks Response:");
+			System.out.println("  " + getAllResponse.getBody());
+			System.out.println("-----------------------------------------------------");
 
-
-
-			//Delete taskid
+			// Delete taskid
 			restTemplate.delete("http://localhost:8080/tasks/delete/" + taskId);
-			System.out.println("Delete Task Response: Task " + taskId + " deleted.");
+			System.out.println("Delete Task Response:");
+			System.out.println("  Task " + taskId + " deleted.");
+			System.out.println("-----------------------------------------------------");
 
 			HttpEntity<String> entity = new HttpEntity<>(headers);
-
-			ResponseEntity<String> deleteResponse = restTemplate.exchange(
-					"http://localhost:8080/users/delete/" + objectid.toString(),
-					HttpMethod.DELETE,
-					entity,
-					String.class);
-
-			System.out.println("Delete Response: " + deleteResponse.getBody());
+			ResponseEntity<String> deleteResponse = restTemplate.exchange("http://localhost:8080/users/delete/" + objectid.toString(), HttpMethod.DELETE, entity, String.class);
+			System.out.println("Delete Response:");
+			System.out.println("  " + deleteResponse.getBody());
+			System.out.println("-----------------------------------------------------");
 		};
 	}
-
 
 }
